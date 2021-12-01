db8dcc690bb9ed136affd8900a7dd3f9d43fe533
soichiro <soichiro@MacBook-Pro.local>
2021-12-02 01:37:15 +0900

    feat(lualine): enable tabline

--- .config/nvim/after/plugin/lualine.rc.lua
+++ .config/nvim/after/plugin/lualine.rc.lua
@@ -4,7 +4,7 @@ if (not status) then return end
 lualine.setup {
   options = {
     icons_enabled = true,
-    theme = 'solarized_dark',
+    theme = 'gruvbox_dark',
     section_separators = {left = '', right = ''},
     component_separators = {left = '', right = ''},
     disabled_filetypes = {}
@@ -37,7 +37,14 @@ lualine.setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
+  tabline = {
+    lualine_a = {'filename'},
+    -- lualine_b = {'branch'},
+    -- lualine_c = {'filename'},
+    lualine_x = {},
+    lualine_y = {},
+    lualine_z = {'tabs'}
+  },
   extensions = {'fugitive'}
 }
 
